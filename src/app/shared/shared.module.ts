9ae25047ee12cf64ba55c756df108849d435e36d
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-01-20 13:13:56 -0500

    Adding sidebar and topbar components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent,
+    TopbarComponent
+  ],
   imports: [
     CommonModule
   ]
