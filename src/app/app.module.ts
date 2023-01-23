f40b51f56614a29248118a4aad6da02bc78896fb
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-01-23 11:01:46 -0500

    Adding data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,18 +10,17 @@ import { SharedModule } from './shared/shared.module';
 import { SummaryModule } from './summary/summary.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     SummaryModule,
     CountryModule,
-    LiveModule
+    LiveModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
