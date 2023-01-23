5f3d60a7dbc5b3d24463ddd3beb3438d68819a1d
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-01-23 10:34:19 -0500

    Creating routes

--- src/app/summary/summary-routing.module.ts
+++ src/app/summary/summary-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SummaryPageComponent } from './components/summary-page/summary-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: SummaryPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SummaryRoutingModule { }
+export class SummaryRoutingModule {}
