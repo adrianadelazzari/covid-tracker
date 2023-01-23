1a905e7ada4bc9ebad792f44452733a55af77501
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-01-23 11:17:45 -0500

    Passing data to summary page component

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getSummaryData() {
+    return this.http.get('https://api.covid19api.com/summary');
+  }
 }
