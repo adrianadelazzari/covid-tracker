de9a515ef0140caeef581632ba658b7be576702e
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-01-31 11:06:15 -0500

    Using number pipe to format the numbers

--- src/app/summary/components/summary-page/summary-page.component.ts
+++ src/app/summary/components/summary-page/summary-page.component.ts
@@ -14,7 +14,6 @@ export class SummaryPageComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.getSummaryData().subscribe((data: any) => {
       this.covidData = data;
-      console.log('Summary Data', data);
     });
   }
 }
